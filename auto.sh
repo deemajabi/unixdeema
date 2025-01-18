518dad530d3192fa9f9c8f0a571a3de4b3fff2f8
deemajabi <dema202073@gmail.com>
2025-01-18 02:53:54 +0200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -4,9 +4,9 @@ git add .
 git commit -m "F1n3lly"
 git push origin main
 
-us="your-user"
-tk="your-token"
-url="your-url"
-jname="your-job"
+us="deemajabi"
+tk="119f829a6a3c30441dc780edb33c01a71e"
+url="http://localhost:8080"
+jname="deploy"
 
 curl -u "$us:$tk" "$url/job/$jname/build=token?$tk"
