a566182739c1d3cd35fb50cabdaf6c12692e2191
deemajabi <dema202073@gmail.com>
2025-01-18 03:32:25 +0200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -9,4 +9,4 @@ tk="119f829a6a3c30441dc780edb33c01a71e"
 url="http://localhost:8080"
 jname="deploy"
 
-curl -u "$us:$tk" "$url/job/$jname/build=token?$tk"
+curl -u "$us:$tk" "$url/job/$jname/build?token=$tk"
